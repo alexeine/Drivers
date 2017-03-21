b0bd1366220df05ce0cb892e4b47941a2b844b53
Brutal <brutalsuperman@gmail.com>
2017-03-21 10:06:02 +0200

    now all working normal

--- src/Drivers/landing/urls.py
+++ src/Drivers/landing/urls.py
@@ -4,10 +4,12 @@ from django.contrib.auth import views as auth_views
 
 from .views import   Main , Action, Choose, Final, Result
 
+
+app_name = 'landing'
 urlpatterns = [
     url(r'^main/choose$', Choose.as_view(), name= 'choose'),
     url(r'^main/$',Main.as_view(), name = 'profile'),
     url(r'^main/action/$' ,Action.as_view(), name='action'),
     url(r'^final/$', Final.as_view(), name='final'),
-    url(r'^main/action/result$', Result.as_view(), name='result'),
+    url(r'^result/$', Result.as_view(), name='result'),
 ]
