7a1b0a3cf499319ae658255e4ab3663b0463e69c
Brutal <brutalsuperman@gmail.com>
2017-03-26 13:20:20 +0300

    sd

--- Drivers/settings.py
+++ Drivers/settings.py
@@ -139,13 +139,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-	#'/home/brutal/Drivers/src/Drivers/static',
 ]
 
-LANGUAGES = (
-    ('en', 'English'),
-    ('ru', 'Русский'),
-)
 
 
 MEDIA_URL = '/media/'
